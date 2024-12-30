1256ed421c1e94e780f0dfb81ca5aa7d15e9788c
aadarsh214 <aadarshgupta7828@gmail.com>
2024-12-31 02:12:52 +0530

    user created

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient
+const prisma = new PrismaClient();
+
+async function insertUser(username : string, password : string, firstName : string, lastName : string, email : string){
+    await prisma.user.create({
+        data: {
+            username,
+            password,
+            firstName,
+            lastName,
+            email
+        }
+    })
+}
+
+insertUser('test', 'test', 'test', 'test', 'test')
